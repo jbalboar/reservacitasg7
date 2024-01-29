074993d45dc6b0ab4ceec90dafa731b0deff7b5c
jbalboar <jbalboa.fiis@gmail.com>
2024-01-28 22:04:53 -0500

    FIRST COMMIT

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module pe.gob.sunat.reservacitasg7 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens pe.gob.sunat.reservacitasg7 to javafx.fxml;
+    exports pe.gob.sunat.reservacitasg7;
+}
---
+++ src/main/java/pe/gob/sunat/reservacitasg7/SecondaryController.java
@@ -0,0 +1,12 @@
+package pe.gob.sunat.reservacitasg7;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
